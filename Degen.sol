6a55bce67e3cf8e9c8a04271334b81679b9f5927
toshiksharma271 <98310467+toshiksharma271@users.noreply.github.com>
2024-06-30 23:25:36 +0530

    Final Challenge Completed

--- Degen.sol
+++ Degen.sol
@@ -1,1 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.25;
 
+contract DegenToken {
+    string public name = "Degen";
+    string public symbol = "DGN";
+    uint public totalSupply;
+    address public owner;
+    string public Redemption_Status;
+
+    mapping(address => uint) public balanceOf;
+
+    modifier onlyOwner() {
+        require(msg.sender == owner, "Only the owner can call this function.");
+        _;
+    }
+
+    constructor(uint initialSupply) {
+        totalSupply = initialSupply;
+        balanceOf[msg.sender] = totalSupply;
+        owner = msg.sender;
+    }
+
+
+    function transfer(address to, uint Amount) external returns (bool) {
+        require(to != address(0), "Invalid recipient address.");
+        require(Amount <= balanceOf[msg.sender], "Insufficient balance.");
+
+        balanceOf[msg.sender] -= Amount;
+        balanceOf[to] += Amount;
+        return true;
+    }
+
+
+    function mint(address to, uint Amount) external onlyOwner {
+        require(to != address(0), "Invalid recipient address.");
+        totalSupply += Amount;
+        balanceOf[to] += Amount;
+    }
+
+    function burn(uint Amount) external {
+        require(Amount <= balanceOf[msg.sender], "Insufficient balance.");
+
+        balanceOf[msg.sender] -= Amount;
+        totalSupply -= Amount;
+    }
+
+    function transferFrom(address from, address to, uint Amount) external returns (bool) {
+        require(to != address(0), "Invalid recipient address.");
+        require(Amount <= balanceOf[from], "Insufficient balance.");
+        balanceOf[from] -= Amount;
+        balanceOf[to] += Amount;
+        return true;
+    }
+
+    function Redeem(uint prizeCost) external {
+        require(balanceOf[msg.sender] >= prizeCost, "Insufficient balance for the prize.");
+        balanceOf[msg.sender] -= prizeCost;
+        Redemption_Status = "Redemption successful!";        
+    }
+}
